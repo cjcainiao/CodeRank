6f6478bf0308f644c1569fb73a006967179786af
大菜鸟 <3136188547@qq.com>
2026-03-30 12:55:12 +0800

    项目创建

---
+++ backend/CodeBox/src/main/java/com/coderank/CodeBoxApplication.java
@@ -0,0 +1,13 @@
+package com.coderank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeBoxApplication.class, args);
+    }
+
+}
---
+++ web/CodeRank/src/router/route.js
@@ -0,0 +1,9 @@
+// 全局路由配置文件
+
+export default routes =[
+    {
+        path:'/',
+        name:'app',
+        component:()=> import('../App.vue')
+    }
+]
